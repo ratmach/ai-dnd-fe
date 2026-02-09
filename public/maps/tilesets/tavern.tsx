b83688f03061f31c09945a2e2a6c94496eedb5cd
rmatchavariani <rmatchavariani@veli.store>
2026-02-09 05:02:00 +0400

    started work on assets

---
+++ public/maps/tilesets/tavern.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tavern_floor" tilewidth="128" tileheight="64" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="13">
+  <image source="../assets/tavern floor.png" width="128" height="64"/>
+ </tile>
+ <tile id="17">
+  <image source="../assets/wallR.png" width="128" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="../assets/wallL.png" width="128" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="../assets/tavernTable.png" width="128" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="../assets/barrels.png" width="128" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="../assets/tavern table long.png" width="128" height="64"/>
+ </tile>
+</tileset>
